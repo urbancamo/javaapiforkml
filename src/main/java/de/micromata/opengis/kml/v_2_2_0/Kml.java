f58b96ccfd54bd4b9bf9015652c1c9d7e18e29d8
Mark Wickens <mark@wickensonline.co.uk>
2023-11-21 16:48:09 +0000

    - preparation for merging Dan's branch (#41)
    Co-authored-by: Mark Wickens <msw@wickensonline.co.uk>

--- src/main/java/de/micromata/opengis/kml/v_2_2_0/Kml.java
+++ src/main/java/de/micromata/opengis/kml/v_2_2_0/Kml.java
@@ -928,38 +928,4 @@ public class Kml implements Cloneable
         }
         return copy;
     }
-
-    private final static class NameSpaceBeautyfier
-        extends NamespacePrefixMapper
-    {
-
-
-        /**
-         * Internal method!
-         * <p>Customizing Namespace Prefixes During Marshalling to a more readable format.</p>
-         * <p>The default output is like:</p>
-         * <pre>{@code&lt;kml ... xmlns:ns2="http://www.w3.org/2005/Atom" xmlns:ns3="urn:oasis:names:tc:ciq:xsdschema:xAL:2.0" xmlns:ns4="http://www.google.com/kml/ext/2.2"&gt;}</pre>
-         * <p>is changed to:</p>
-         * <pre>{@code &lt;kml ... xmlns:atom="http://www.w3.org/2005/Atom" xmlns:xal="urn:oasis:names:tc:ciq:xsdschema:xAL:2.0" xmlns:gx="http://www.google.com/kml/ext/2.2"&gt;}</pre><p>What it does:</p>
-         * <p>namespaceUri: http://www.w3.org/2005/Atom              prefix: atom</p><p>namespaceUri: urn:oasis:names:tc:ciq:xsdschema:xAL:2.0 prefix: xal</p><p>namespaceUri: http://www.google.com/kml/ext/2.2        prefix: gx</p><p>namespaceUri: anything else prefix: null</p>
-         * 
-         */
-        @Override
-        public String getPreferredPrefix(String namespaceUri, String suggestion, boolean requirePrefix) {
-            if (namespaceUri.matches("http://www.opengis.net/kml/.*?")) {
-                return "";
-            }
-            if (namespaceUri.matches("http://www.w3.org/\\d{4}/Atom")) {
-                return "atom";
-            }
-            if (namespaceUri.matches("urn:oasis:names:tc:ciq:xsdschema:xAL:.*?")) {
-                return "xal";
-            }
-            if (namespaceUri.matches("http://www.google.com/kml/ext/.*?")) {
-                return "gx";
-            }
-            return null;
-        }
-
-    }
 }
